a6a1c1595c83dfe4db07beb8b432e0a9225b1dca
Dulanga Wimalagunasekara <it22917270@my.sliit.lk>
2025-12-30 15:47:41 +0530

    Minor Changes

--- src/neo4j.js
+++ src/neo4j.js
@@ -31,12 +31,12 @@ const SNAPSHOT_QUERY = `
 UNWIND $batch AS row
 MERGE (a:Service {serviceId: row.sourceId})
   ON CREATE SET a.name = row.sourceName, a.namespace = row.sourceNamespace, a.createdAt = datetime(),
-                a.podCount = row.sourcePodCount, a.availability = row.sourceAvailability
+                a.updatedAt = datetime(), a.podCount = row.sourcePodCount, a.availability = row.sourceAvailability
   ON MATCH SET a.updatedAt = datetime(),
                a.podCount = row.sourcePodCount, a.availability = row.sourceAvailability
 MERGE (b:Service {serviceId: row.destId})
   ON CREATE SET b.name = row.destName, b.namespace = row.destNamespace, b.createdAt = datetime(),
-                b.podCount = row.destPodCount, b.availability = row.destAvailability
+                b.updatedAt = datetime(), b.podCount = row.destPodCount, b.availability = row.destAvailability
   ON MATCH SET b.updatedAt = datetime(),
                b.podCount = row.destPodCount, b.availability = row.destAvailability
 MERGE (a)-[r:CALLS_NOW]->(b)
